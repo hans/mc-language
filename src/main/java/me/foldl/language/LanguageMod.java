2864aa796b111b39a416a0d778620d158a3236ef
Jon Gauthier <jon@gauthiers.net>
2021-03-10 15:06:07 -0500

    raytrace cursor and fire when entry block under cursor

--- src/main/java/me/foldl/language/LanguageMod.java
+++ src/main/java/me/foldl/language/LanguageMod.java
@@ -6,18 +6,20 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Material;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.world.biome.Biome;
 
 public class LanguageMod implements ModInitializer {
 	public static final Block ENTRY_BLOCK = new EntryBlock(FabricBlockSettings.of(Material.METAL).strength(4.0f));
@@ -57,8 +59,19 @@ public class LanguageMod implements ModInitializer {
 
 			i[0]++;
 		});
-		ClientTickEvents.END_WORLD_TICK.register((ClientWorld world) -> {
 
+		ClientTickEvents.END_WORLD_TICK.register((ClientWorld world) -> {
+			// Check for block under crosshairs.
+			MinecraftClient client = MinecraftClient.getInstance();
+			HitResult hit = client.crosshairTarget;
+			if (hit.getType() == HitResult.Type.BLOCK) {
+				BlockPos pos = ((BlockHitResult) hit).getBlockPos();
+				BlockState blockState = world.getBlockState(pos);
+				Block block = blockState.getBlock();
+				if (block instanceof EntryBlock) {
+					System.out.println("block");
+				}
+			}
 		});
 	}
 }
