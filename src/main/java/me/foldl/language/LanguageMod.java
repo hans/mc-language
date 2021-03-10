97ce5b1d39d5b77cce46f9643cb8b02180d7c378
Jon Gauthier <jon@gauthiers.net>
2021-03-10 13:12:22 -0500

    get weather trigger working, yay!

--- src/main/java/me/foldl/language/LanguageMod.java
+++ src/main/java/me/foldl/language/LanguageMod.java
@@ -2,6 +2,7 @@ package me.foldl.language;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
@@ -11,9 +12,12 @@ import net.minecraft.client.world.ClientWorld;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.biome.Biome;
 
 public class LanguageMod implements ModInitializer {
 	public static final Block ENTRY_BLOCK = new EntryBlock(FabricBlockSettings.of(Material.METAL).strength(4.0f));
@@ -32,7 +36,18 @@ public class LanguageMod implements ModInitializer {
 		ENTRY_BLOCK_ITEM = Registry.register(Registry.ITEM, new Identifier("language", "entry_block"),
 				new BlockItem(ENTRY_BLOCK, new FabricItemSettings().group(ItemGroup.MISC)));
 
+		final int[] i = new int[] {0};
+		ServerTickEvents.END_WORLD_TICK.register((ServerWorld world) -> {
+			System.out.println(i[0]);
+			if (i[0] % 100 == 0) {
+				System.out.println("starting RAIN");
+				world.setWeather(0, 24000, true, true);
+			}
+
+			i[0]++;
+		});
 		ClientTickEvents.END_WORLD_TICK.register((ClientWorld world) -> {
+
 			System.out.println("end world tick");
 		});
 	}
